fd42bfdc146e444ae1fdac201c355175f1d2efac
benjaminsinaiko <ben.jamin@outlook.com>
2020-05-28 13:14:59 -0500

    Update layout, styling of movie card

--- src/components/movieDisplay/MovieCard.js
+++ src/components/movieDisplay/MovieCard.js
@@ -3,16 +3,35 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import HistoryIcon from '@material-ui/icons/History';
+import SkipNextIcon from '@material-ui/icons/SkipNext';
 import Skeleton from '@material-ui/lab/Skeleton';
 
 import { useMovies } from '../../contexts/moviesContext';
-import BottomActions from './BottomActions';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    width: '100%',
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  movieContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  sideButtons: {
+    height: '95vh',
+    minWidth: '5vw',
+    padding: 0,
+  },
+  cardContainer: {
     width: '90vw',
     maxWidth: 1000,
-    height: '70vh',
+    height: '95vh',
     background: theme.palette.warning.light,
   },
   media: {
@@ -36,6 +55,15 @@ const useStyles = makeStyles((theme) => ({
     alignItems: 'center',
     color: 'transparent',
     textShadow: '0 0 5px rgba(0,0,0,0.6)',
+  },
+  bottomButtonContainer: {
+    width: '100%',
+    maxWidth: 1000,
+    textAlign: 'center',
+  },
+  showMovieButton: {
+    alignSelf: 'flex-end',
+    width: '100%',
   },
   loadingSkeleton: {
     display: 'flex',
@@ -64,36 +92,54 @@ export default function MediaCard({ openDrawer }) {
   }
 
   return (
-    <div>
-      <Card className={classes.root}>
-        {!state.movie ? (
-          <div className={classes.loadingSkeleton}>
-            <Skeleton variant='rect' width='90%' height='80%' className={classes.skeleton} />
-            <Skeleton variant='text' width='90%' height='20%' className={classes.skeleton} />
-          </div>
-        ) : (
-          <>
-            <CardMedia className={classes.media} image={state.image} title='Movie Image' />
-            <div className={classes.movieInfo}>
-              {isDisplayMovie ? (
-                <Typography variant='h4' align='center' className={classes.movieTitle}>
-                  {state.movie}
-                </Typography>
-              ) : (
-                <Typography variant='h3' className={classes.blurryText}>
-                  ? ? ? ? ? ? ? ? ? ?
-                </Typography>
-              )}
+    <div className={classes.root}>
+      <div className={classes.movieContainer}>
+        <Button
+          variant='contained'
+          color='secondary'
+          className={classes.sideButtons}
+          onClick={openDrawer}
+        >
+          <HistoryIcon style={{ fontSize: 40 }} />
+        </Button>
+
+        <Card className={classes.cardContainer}>
+          {!state.movie ? (
+            <div className={classes.loadingSkeleton}>
+              <Skeleton variant='rect' width='90%' height='80%' className={classes.skeleton} />
+              <Skeleton variant='text' width='90%' height='20%' className={classes.skeleton} />
             </div>
-          </>
-        )}
-      </Card>
-      <BottomActions
-        detailsShown={isDisplayMovie}
-        showMovieDetails={handleShowDetails}
-        fetchNextMovie={fetchNextMovie}
-        openDrawer={openDrawer}
-      />
+          ) : (
+            <>
+              <CardMedia className={classes.media} image={state.image} title='Movie Image' />
+              <div className={classes.movieInfo}>
+                {isDisplayMovie ? (
+                  <Typography variant='h4' align='center' className={classes.movieTitle}>
+                    {state.movie}
+                  </Typography>
+                ) : (
+                  <Typography
+                    variant='h3'
+                    className={classes.blurryText}
+                    onClick={handleShowDetails}
+                  >
+                    ? ? ? ? ? ? ? ? ? ?
+                  </Typography>
+                )}
+              </div>
+            </>
+          )}
+        </Card>
+
+        <Button
+          variant='contained'
+          color='primary'
+          className={classes.sideButtons}
+          onClick={fetchNextMovie}
+        >
+          <SkipNextIcon style={{ fontSize: 40 }} />
+        </Button>
+      </div>
     </div>
   );
 }
